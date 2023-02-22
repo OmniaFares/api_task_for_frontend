205e1059214fde5b5d38b4f6b05789f23c292d3b
OmniaFares <OFaris@MEDICONSULT3.COM>
2023-02-22 14:09:49 +0200

    test

--- api/models.py
+++ api/models.py
@@ -18,6 +18,7 @@ class Category(models.Model):
         return self.name
 
 
+
 class Brand(models.Model):
     sale_percentage = models.FloatField()
     image = models.ImageField(upload_to='api/images/brands/')
