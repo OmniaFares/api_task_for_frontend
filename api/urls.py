ac3ae781dc5164215a1dc71bd0ee4febcfa62fa3
OmniaFares <omniafares133@gmail.com>
2023-02-22 14:43:01 +0200

    documentation

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from .views import SliderImagesViewSet, CategoryViewSet, BrandViewSet, ItemsViewSet
 
 router = DefaultRouter()
@@ -7,7 +10,21 @@ router.register(r'categories', CategoryViewSet)
 router.register(r'brands', BrandViewSet)
 router.register(r'items', ItemsViewSet)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="testing@api.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('slider_image', SliderImagesViewSet.as_view({'get': 'get_image'})),
     path('', include(router.urls)),
+    path('docs', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
--- frontend_api/settings.py
+++ frontend_api/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'frontend_api',
-    'api'
+    'api',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
