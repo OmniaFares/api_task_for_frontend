3dc1264cde950adf9b99613b5e2d603f42bdc04d
OmniaFares <omniafares133@gmail.com>
2023-02-27 14:05:51 +0200

    setting up files

--- api/views.py
+++ api/views.py
@@ -6,33 +6,37 @@ from .serializer import SliderImageSerializer, CategorySerializer, BrandSerializ
 
 
 class SliderImagesViewSet(ViewSet):
+    queryset = SliderImage.objects.order_by('?').first()
+    serializer = SliderImageSerializer
 
     @action(detail=True, methods=['get'])
     def get_image(self, request):
-        item = SliderImage.objects.order_by('?').first()
-        serializer = SliderImageSerializer(item)
+        serializer = self.serializer(self.queryset)
         return Response(serializer.data)
     
 
 class CategoryViewSet(ViewSet):
     queryset = Category.objects.all()
+    serializer = CategorySerializer
 
     def list(self, request):
-        serializer = CategorySerializer(self.queryset, many=True)
+        serializer = self.serializer(self.queryset, many=True)
         return Response(serializer.data)
     
 
 class BrandViewSet(ViewSet):
     queryset = Brand.objects.all()
+    serializer = BrandSerializer
 
     def list(self, request):
-        serializer = BrandSerializer(self.queryset, many=True)
+        serializer = self.serializer(self.queryset, many=True)
         return Response(serializer.data)
 
 
 class ItemsViewSet(ViewSet):
     queryset = Item.objects.all()
+    serializer = ItemSerializer
 
     def list(self, request):
-        serializer = ItemSerializer(self.queryset, many=True)
+        serializer = self.serializer(self.queryset, many=True)
         return Response(serializer.data)
